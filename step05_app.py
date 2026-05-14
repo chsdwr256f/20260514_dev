8821a6ffdaa209308a0220780be83ae20fd3a8c5
chsdwr256f <160517760+chsdwr256f@users.noreply.github.com>
2026-05-14 13:18:59 +0100

    Update step05_app.py

--- step05_app.py
+++ step05_app.py
@@ -10,10 +10,15 @@ import streamlit as st
 from rdflib import Graph, Literal, URIRef
 from rdflib.namespace import OWL, RDF, RDFS, DCTERMS
 
+OLLAMA_AVAILABLE = False
+
 try:
     import ollama
+
+    ollama.list()
     OLLAMA_AVAILABLE = True
-except ImportError:
+
+except Exception:
     OLLAMA_AVAILABLE = False
 
 try:
